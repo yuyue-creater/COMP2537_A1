07d44f7d191920d7d502443ca207bd5f62028216
yuyue-creater <59495230+yuyue-creater@users.noreply.github.com>
2023-04-27 11:54:47 -0700

    Add files via upload

---
+++ bcrypt.js
@@ -0,0 +1,8 @@
+const bcrypt = require('bcrypt')
+const plainPassword = '123456';
+
+const encryptedPassword = bcrypt.hashSync(plainPassword, 10);
+console.log(encryptedPassword) // $2b$10$GCQyK4d8vHkuWld0Zt59nOlSJbFj3QLj7bZtaqxj0iJFdcAywRg6y
+console.log(
+    bcrypt.compareSync("123456", encryptedPassword)
+)
