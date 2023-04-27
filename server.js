af95504036d3a284b35e00981da217909c39a744
yuyue-creater <59495230+yuyue-creater@users.noreply.github.com>
2023-04-27 11:56:00 -0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const app = require('./app');
+const dotenv = require('dotenv');
+dotenv.config();
+// require('dotenv').config()
+
+main().catch(err => console.log(err));
+async function main() {
+  //await mongoose.connect(`mongodb+srv://michaelyuyue2012:wxWUA68uy81WGiYh@cluster0.wvnpat0.mongodb.net/comp2537w1?retryWrites=true&w=majority`);
+  await mongoose.connect('mongodb://127.0.0.1:27017/comp2537w1');
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  console.log("connected to db");
+  app.listen(process.env.PORT || 3000, () => {
+    console.log('server is running on port 3000');
+  });
+}
+
+
+
+
+// mongodb+srv://michaelyuyue2012:wxWUA68uy81WGiYh@cluster0.wvnpat0.mongodb.net/?retryWrites=true&w=majority
+// wxWUA68uy81WGiYh
