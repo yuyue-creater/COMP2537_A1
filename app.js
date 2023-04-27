3659569d947798b4af2c6e79d171a21fd7fb9222
yuyue-creater <michaelyuyue2012@gmail.com>
2023-04-27 13:48:18 -0700

    commited

--- app.js
+++ app.js
@@ -19,9 +19,8 @@ var dbStore = new MongoDBStore({
 })
 
 
-
 const expireTime = 3600;
-// 24 * 60 * 60 * 1000; //expires after 1 hour  (hours * minutes * seconds * millis)
+//24 * 60 * 60 * 1000; //expires after 1 hour  (hours * minutes * seconds * millis)
 
 
 // replace the in-memory array session store with a database session store
@@ -58,8 +57,6 @@ app.get('/', (req, res) => {
     res.send(html)
 })
 
-
-
 app.get('/signup', (req, res) => {
     var html = `
     create user
@@ -81,6 +78,11 @@ app.post('/submitUser', async (req, res) => {
     var username = req.body.username;
     var password = req.body.password;
 
+    if (username == "" || password == "") {
+        res.redirect("/signupSubmit")
+        return
+    }
+
     const schema = Joi.object(
         {
             username: Joi.string().alphanum().max(20).required(),
@@ -88,23 +90,33 @@ app.post('/submitUser', async (req, res) => {
         });
 
     const validationResult = schema.validate({ username, password });
-    if (validationResult.error != null) {
+    if (validationResult.error) {
         console.log(validationResult.error);
         res.redirect("/signup");
         return;
     }
 
-    var userPassword = await bcrypt.hash(password, 10);
+    var userPassword = await bcrypt.hash(password, 12);
 
     await usersModel.insertMany([{ username: username, password: userPassword }]);
     req.session.GLOBAL_AUTHENTICATED = true;
-    req.session.username = username;
+    // req.session.username = username;
     req.session.cookie.maxAge = expireTime;
 
     console.log("Inserted user");
 
     res.redirect('/members');
 });
+
+app.get('/signupSubmit', (req, res) => {
+    res.send(`
+        Name, email, and password are all required
+        <form action="/signup" method="get">
+        <input type="submit" value="Try again"/>
+        </form>
+    `)
+})
+
 
 
 // loggedina
@@ -121,8 +133,6 @@ app.get('/login', (req, res) => {
         </form>
     `)
 });
-app.use(express.urlencoded({ extended: false }))
-// built-in middleware function in Express. It parses incoming requests with urlencoded payloads and is based on body-parser.
 
 app.post('/loggingin', async (req, res) => {
     var username = req.body.username;
@@ -141,7 +151,7 @@ app.post('/loggingin', async (req, res) => {
     console.log(result);
     if (!result) {
         console.log("user not found");
-        res.redirect("/login");
+        res.redirect("/invalid");
         return;
     }
     if (await bcrypt.compare(password, result.password)) {
@@ -149,17 +159,24 @@ app.post('/loggingin', async (req, res) => {
         req.session.GLOBAL_AUTHENTICATED = true;
         req.session.username = username;
         req.session.cookie.maxAge = expireTime;
-
         res.redirect('/Members');
         return;
     }
     else {
-        console.log("incorrect password");
-        res.redirect("/login");
+
+        res.redirect("/invalid");
         return;
     }
 });
 
+app.get('/invalid', (req, res) => {
+    res.send(`
+        Invalid username/password combination
+        <form action="/login" method="get">
+        <input type="submit" value="Try again"/>
+        </form>
+    `)
+})
 // app.post('/loggedina', async (req, res) => {
 //     // set a global variable to true if the user is authenticated
 //     try {
@@ -182,15 +199,6 @@ app.post('/loggingin', async (req, res) => {
 
 
 
-// authenticated users only
-const authenticatedOnly = (req, res, next) => {
-    if (!req.session.GLOBAL_AUTHENTICATED) {
-        return res.status(401).json({ error: 'not authenticated' });
-    }
-    next(); // allow the next route to run
-};
-
-app.use(authenticatedOnly);
 
 app.use(express.static('public'))
 
@@ -216,26 +224,36 @@ app.get('*', (req, res) => {
     res.status(404).send('<h1> 404 Page not found</h1>');
 });
 
-const protectedRouteForAdminsOnlyMiddlewareFunction = async (req, res, next) => {
-    try {
-        const result = await usersModel.findOne(
-            { 
-                username: req.session.loggedUsername,
-                password: req.session.loggedPassword
-             }
-        )
-        if (result?.type != 'administrator') {
-            return res.send('<h1> You are not an admin </h1>')
-        }
-        next(); // allow the next route to run
-    } catch (error) {
-        console.log(error);
+// authenticated users only
+const authenticatedOnly = (req, res, next) => {
+    if (!req.session.GLOBAL_AUTHENTICATED) {
+        return res.status(401).json({ error: 'not authenticated' });
     }
+    next(); // allow the next route to run
 };
-app.use(protectedRouteForAdminsOnlyMiddlewareFunction);
-app.get('/protectedRouteForAdminsOnly', (req, res) => {
-    res.send('<h1> protectedRouteForAdminsOnly </h1>');
-});
+
+app.use(authenticatedOnly);
+
+// const protectedRouteForAdminsOnlyMiddlewareFunction = async (req, res, next) => {
+//     try {
+//         const result = await usersModel.findOne(
+//             { 
+//                 username: req.session.loggedUsername,
+//                 password: req.session.loggedPassword
+//              }
+//         )
+//         if (result?.type != 'administrator') {
+//             return res.send('<h1> You are not an admin </h1>')
+//         }
+//         next(); // allow the next route to run
+//     } catch (error) {
+//         console.log(error);
+//     }
+// };
+// app.use(protectedRouteForAdminsOnlyMiddlewareFunction);
+// app.get('/protectedRouteForAdminsOnly', (req, res) => {
+//     res.send('<h1> protectedRouteForAdminsOnly </h1>');
+// });
 
 
 
--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 const dotenv = require('dotenv');
-dotenv.config();
+dotenv.config({});
 // require('dotenv').config()
+
 
 main().catch(err => console.log(err));
 async function main() {
-  //await mongoose.connect(`mongodb+srv://michaelyuyue2012:wxWUA68uy81WGiYh@cluster0.wvnpat0.mongodb.net/comp2537w1?retryWrites=true&w=majority`);
-  await mongoose.connect('mongodb://127.0.0.1:27017/comp2537w1');
+  await mongoose.connect(`mongodb+srv://${process.env.ATLAS_DB_USER}:${process.env.ATLAS_DB_PASSWORD}@cluster0.wvnpat0.mongodb.net/comp2537w1?retryWrites=true&w=majority`);
+  //await mongoose.connect('mongodb://127.0.0.1:27017/comp2537w1');
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
   console.log("connected to db");
   app.listen(process.env.PORT || 3000, () => {
@@ -17,6 +18,5 @@ async function main() {
 
 
 
-
-// mongodb+srv://michaelyuyue2012:wxWUA68uy81WGiYh@cluster0.wvnpat0.mongodb.net/?retryWrites=true&w=majority
-// wxWUA68uy81WGiYh
+// mongodb+srv://michaelyuyue2012:bw5kZzYkHBkrrxFf@cluster0.wvnpat0.mongodb.net/?retryWrites=true&w=majority
+// bw5kZzYkHBkrrxFf
