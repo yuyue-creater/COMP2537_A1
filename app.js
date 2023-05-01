eabdc70a9c41cc860d9cae6543b8be5d657b17a8
yuyue-creater <michaelyuyue2012@gmail.com>
2023-04-30 21:00:14 -0700

    App updated

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const app = express();
 const session = require('express-session')
 const usersModel = require('./models/w1users');
 const bcrypt = require('bcrypt');
-
 const Joi = require("joi");
 const dotenv = require('dotenv')
 dotenv.config();
@@ -16,23 +15,20 @@ const node_session_secret = process.env.NODE_SESSION_SECRET;
 /* END secret section */
 
 
-
-
 var dbStore = new MongoDBStore({
     // uri: 'mongodb://localhost:27017/connect_mongodb_session_test',
     uri: `mongodb+srv://${process.env.ATLAS_DB_USER}:${process.env.ATLAS_DB_PASSWORD}@${process.env.ATLAS_DB_HOST}/comp2537w1?retryWrites=true&w=majority`,
     collection: 'Sessions',
     crypto: {
-		secret: mongodb_session_secret
-	}
-  });
-  
+        secret: mongodb_session_secret
+    }
+});
+
 
 const expireTime = 1 * 60 * 60 * 1000; //expires after 1 hour  (hours * minutes * seconds * millis)
 
 app.use(express.urlencoded({ extended: false }))
 
-// replace the in-memory array session store with a database session store
 app.use(session({
     secret: node_session_secret,
     store: dbStore,
@@ -87,14 +83,29 @@ app.post('/submitUser', async (req, res) => {
     var email = req.body.email;
 
     if (username == "" || password == "" || email == "") {
-        res.redirect("/signupSubmit")
+        var message = ``
+        if (username == "") {
+            message += `<p>Name is missing</p><br>`
+            
+        }
+        if (email == "") {
+            message += `<p>Email is missing</p><br>`
+        }
+        if (password == "") {
+            message += '<p>Password is missing</p><br>'
+        }
+        message += `Name, email, and password are all required
+    <form action='/signup' method='get'>
+    <input type='submit' value='Try again'/>
+    </form>`
+        res.send(message)
         return
     }
 
-    const username_test = Joi.string().min(3).max(30).required();
-    const password_test = Joi.string().max(20).required()
-
+    const username_test = Joi.string().alphanum().min(3).max(30).required();
     const username_result = username_test.validate(username);
+
+    const password_test = Joi.string().alphanum().min(5).max(20).required()
     const password_result = password_test.validate(password)
 
     if (username_result.error || password_result.error) {
@@ -104,26 +115,14 @@ app.post('/submitUser', async (req, res) => {
     }
 
     var userPassword = await bcrypt.hash(password, 12);
-
     await usersModel.insertMany([{ username: username, password: userPassword }]);
     req.session.GLOBAL_AUTHENTICATED = true;
     req.session.username = username;
     req.session.password = userPassword;
-    req.session.cookie.maxAge = expireTime;
-
     console.log("Inserted user");
-    console.log(req.session.cookie.maxAge)
     res.redirect('/members');
 });
 
-app.get('/signupSubmit', (req, res) => {
-    
-    res.send(`
-        Name, email, and password are all required
-        <form action="/signup" method="get">
-        <input type="submit" value="Try again"/>
-        </form>`)
-})
 
 
 app.get('/login', (req, res) => {
@@ -155,31 +154,31 @@ app.post('/loggingin', async (req, res) => {
     const result = await usersModel.findOne({ username: username })
     if (!result) {
         console.log("user not found");
-        res.redirect("/invalid");
+        res.send(`
+        User is not found
+        <form action="/login" method="get">
+        <input type="submit" value="Try again"/>
+        </form>
+    `)
         return;
     }
     if (await bcrypt.compare(password, result.password)) {
         req.session.GLOBAL_AUTHENTICATED = true;
         req.session.username = username;
-        req.session.cookie.maxAge = expireTime;
         res.redirect('/Members');
         return;
     }
     else {
         console.log('wrong password')
-        res.redirect("/invalid");
-        return;
-    }
-});
-
-app.get('/invalid', (req, res) => {
-    res.send(`
+        res.send(`
         Invalid username/password combination
         <form action="/login" method="get">
         <input type="submit" value="Try again"/>
         </form>
     `)
-})
+        return;
+    }
+});
 
 app.use(express.static('public'))
 
@@ -199,8 +198,6 @@ app.get('/Members', (req, res) => {
         res.redirect('/');
         return
     }
-    
-    
 });
 
 app.get('/logout', (req, res) => {
@@ -221,6 +218,5 @@ const authenticatedOnly = (req, res, next) => {
 };
 
 app.use(authenticatedOnly);
-
 
 module.exports = app;
